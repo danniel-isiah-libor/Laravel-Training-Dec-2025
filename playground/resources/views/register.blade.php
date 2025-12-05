00a2e3dc70fb8a12a7c9a7e6e109994582df8a5b
Caryl Dueñas <ciduenas@sec.gov.ph>
2025-12-05 14:05:47 +0800

    test

--- playground/resources/views/register.blade.php
+++ playground/resources/views/register.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
-        {{-- @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @else
             <style>
@@ -1516,34 +1516,76 @@
                     initial-value: ""
                 }
             </style>
-        @endif --}}
+        @endif
     </head>
 
     <body class="h-full">
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-            <x-title label="Register to your account" />
-
-            <x-quote />
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company" class="mx-auto h-10 w-auto" />
+                <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Register to your account
+                </h2>
+            </div>
 
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form action="{{ route('register.store') }}" method="POST" class="space-y-6">
                     @csrf
 
-                    <x-form.input-field name="name" label="Name" />
+                    <div>
+                        <label for="name" class="block text-sm/6 font-medium text-gray-100">Name</label>
+                        <div class="mt-2">
+                            <input id="name" type="text" name="name" required autocomplete="name"
+                                class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                        </div>
 
-                    <x-form.input-field name="email" label="Email" type="email" />
+                        @error('name')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
 
-                    <x-form.input-field name="password" label="Password" type="password" />
+                    <div>
+                        <label for="email" class="block text-sm/6 font-medium text-gray-100">Email address</label>
+                        <div class="mt-2">
+                            <input id="email" type="email" name="email" required autocomplete="email"
+                                class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                        </div>
 
-                    <x-form.input-field name="password_confirmation" label="Confirm Password" type="password" />
+                        @error('email')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
 
-                    <x-button label="Login" />
+                    <div>
+                        <label for="password" class="block text-sm/6 font-medium text-gray-100">Password</label>
+                        <div class="mt-2">
+                            <input id="password" type="text" name="password" required
+                                class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                        </div>
+
+                        @error('password')
+                            <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <label for="password_confirmation" class="block text-sm/6 font-medium text-gray-100">Password
+                            Confirmation</label>
+                        <div class="mt-2">
+                            <input id="password_confirmation" type="text" name="password_confirmation" required
+                                class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit"
+                            class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Register</button>
+                    </div>
                 </form>
 
                 <p class="mt-10 text-center text-sm/6 text-gray-400">
                     Already a member?
-                    <a href="{{ route('login') }}" class="font-semibold text-indigo-400 hover:text-indigo-300">Log
-                        in</a>
+                    <a href="#" class="font-semibold text-indigo-400 hover:text-indigo-300">Log in</a>
                 </p>
             </div>
         </div>
