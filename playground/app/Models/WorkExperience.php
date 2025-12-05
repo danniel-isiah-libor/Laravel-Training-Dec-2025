31cff93ebf77aad3d6d0e606e529ca6083113c5a
Danniel Libor <dlibor.dev@gmail.com>
2025-12-05 15:55:46 +0800

    feat(work-experience): implement WorkExperience model, factory, seeder, and migrations

--- playground/app/Models/WorkExperience.php
+++ playground/app/Models/WorkExperience.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class WorkExperience extends Model
 {
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'uuid',
+        'company_name',
+        'position',
+        'start_date',
+        'end_date',
+    ];
+
     public static function getData()
     {
         return [
